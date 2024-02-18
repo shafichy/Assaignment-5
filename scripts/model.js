eeecc27c6236f10dda090cbf84c4634c310b3d96
shafichy <shafichy1400@gmail.com>
2024-02-18 22:55:07 +0600

    added some section

--- scripts/model.js
+++ scripts/model.js
@@ -13,7 +13,7 @@
 
 function setBackgroundColor(elementId){
     element = document.getElementById(elementId);
-    element.classList.add('bg-orange-400')
+    element.classList.add('bg-green-500')
 }  
 
 function setInnerText(elementId, value){
@@ -26,15 +26,17 @@ function color(elementId){
     element.classList.add('text-stone-50');
 }
 
+
 const allSeat = document.getElementsByClassName('seat-part')
 let count = 0;
 let count1 = 40;
 let price = 0;
 let price1 =0;
+let sum = 0;
 for(const seat of allSeat){
     seat.addEventListener('click', function(e){
       
-        
+        if(sum < 4){
         const seatColor = e.target.innerText;
         setBackgroundColor(seatColor);
 
@@ -47,39 +49,119 @@ for(const seat of allSeat){
         setInnerText('seat-decrease', count1);
        
 
-      const cartContainer = document.getElementById('cart-container');
+      const cartContainer = document.getElementById('cart-container1');
 
-      const li = document.createElement('li');
+   
      
       
       const p = document.createElement('p');
       p.innerText = e.target.innerText;
-      console.log(p);
       
+      cartContainer.appendChild(p);
+   
+      
+    const cartContainer1 = document.getElementById('cart-container2')
       const p1 = document.createElement('p');
       p1.innerText = 'Economy';
-  console.log(p1);
+  cartContainer1.appendChild(p1);
+
+  const cartContainer2 = document.getElementById('cart-container3')
       const p2 = document.createElement('p');
       p2.innerText = '550';
+      cartContainer2.appendChild(p2);
 
-      li.appendChild(p);
-      li.appendChild(p1);
-      li.appendChild(p2);
-       
-      cartContainer.appendChild(li);
+
 
         price+=550;
         setInnerText('ticket-cost', price);
         
         price1+=550;
         setInnerText('grand-cost', price1);
+        sum++;
+        if(sum === 4){
+            return sum;
+        }
+        }
+
+        else{
+            alert('You have booked already 4 seat');
+        }
 
 
-       
-    //    const AllSeat = randomSeat();
-    //    console.log(AllSeat.innerText);
 
-    //    setBackgroundColor(AllSeat.innerText);
 
     })
 }
+
+
+const applyButton = document.getElementById('apply-btn');
+
+applyButton.addEventListener('click', function(e){
+
+if(price < 2200){
+ applyButton.disabled = true;
+}
+else{
+  const inputCupon = document.getElementById('input-field').value;
+
+   if(price >= 2200){
+    if(inputCupon === "NEW15"){
+      const discountPrice = price * 0.15;
+
+      const discountElement = document.getElementById('discount-price1');
+
+    const discountText = document.getElementById('discount-price2');
+
+    const discountText1 = document.getElementById('discount-price3')
+
+    const p = document.createElement('p');
+    p.innerText = 'Discount Price';
+    discountElement.appendChild(p);
+
+
+    const p3 = document.createElement('p');
+    p3.innerText = 'BDT'
+    discountText1.appendChild(p3);
+
+    const p1 = document.createElement('p');
+    p1.innerText = discountPrice;
+    discountText.appendChild(p1);
+
+    const grandTotal = document.getElementById('grand-cost');
+    grandTotal.innerText = price - discountPrice;
+    }
+    else if(inputCupon === "Couple 20")
+    {
+        const discountPrice = price * 0.2;
+
+        const discountElement = document.getElementById('discount-price1');
+  
+      const discountText = document.getElementById('discount-price2');
+  
+      const discountText1 = document.getElementById('discount-price3')
+  
+      const p = document.createElement('p');
+      p.innerText = 'Discount Price';
+      discountElement.appendChild(p);
+  
+  
+      const p3 = document.createElement('p');
+      p3.innerText = 'BDT'
+      discountText1.appendChild(p3);
+  
+      const p1 = document.createElement('p');
+      p1.innerText = discountPrice;
+      discountText.appendChild(p1);
+
+      const grandTotal = document.getElementById('grand-cost');
+      grandTotal.innerText = price - discountPrice;
+    }
+    else{
+        alert("Invalid Coupone");
+       }
+   }
+   else{
+    alert("PLease booking atleast 4 seats");
+   }
+}
+})
